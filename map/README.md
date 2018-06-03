dbe561404a12d9bdf335f674f930710403850e6c
lifeinbofyx@sina.com <lifeinbofyx@sina.com>
2018-06-03 11:13:01 +0800

    NU
    NU

--- map/README.md
+++ map/README.md
@@ -1,1 +1,5 @@
         1.创造一个可以被标记的map
+        2.增加添加地理位置功能
+
+        2.参考资料： leafletjs
+                    https://codepen.io/mgrubinger/pen/bNGLLY   翻页效果
--- map/index.js
+++ map/index.js
@@ -1,5 +1,5 @@
 var mymap = L.map('leafletmap');
-mymap.setView([40.775,-73.972], 15);
+mymap.setView([32.9949148921,115.4957485199], 15);
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
     maxZoom: 18
